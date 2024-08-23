0413ac5ae1bc4906785d79b1b1fe8b8d22e691a4
DavidsTens <134627046+DavidsTens@users.noreply.github.com>
2024-08-23 07:22:08 +0300

    Update installarch.sh

--- installarch.sh
+++ installarch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Запрос данных для переменных
-read -p "Введите имя будущего пользователя (USER): " USER
-read -p "Введите имя компьютера (HOST): " HOST
-read -p "Введите путь до раздела диска для установки (например, /dev/sda1): " PATH
+read -p "username (USER): " USER
+read -p "hostname (HOST): " HOST
+read -p "path to install (e.g., /dev/sda1): " PATH
 
-sudo pacman -Sy
-sudo pacman -S archlinux-keyring
+pacman -Sy
+pacman -S archlinux-keyring
 
 mkfs.btrfs -L arch $PATH
 mount $PATH /mnt
@@ -21,21 +21,21 @@ mkdir /mnt/home
 mount -o subvol=/@home,noatime,compress=lzo $PATH /mnt/home
 
 echo "liquorix"
-sudo pacman-key --keyserver hkps://keyserver.ubuntu.com --recv-keys 9AE4078033F8024D
-sudo pacman-key --lsign-key 9AE4078033F8024D
+pacman-key --keyserver hkps://keyserver.ubuntu.com --recv-keys 9AE4078033F8024D
+pacman-key --lsign-key 9AE4078033F8024D
 
 echo -e '\n[liquorix]\nServer = https://liquorix.net/archlinux/$repo/$arch' | sudo tee -a '/etc/pacman.conf' > /dev/null
 
 echo "chaotic-aur"
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key 3056513887B78AEB
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
 
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a '/etc/pacman.conf' > /dev/null
 
-sudo pacman -Sy
+pacman -Sy
 
 pacstrap /mnt base base-devel linux-lqx linux-lqx-headers linux-firmware networkmanager intel-ucode git nano curl wget
 genfstab -U /mnt >> /mnt/etc/fstab

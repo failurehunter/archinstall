52eff1f2038e2f220fb6cd1f0a495c60a84e34a7
DavidsTens <134627046+DavidsTens@users.noreply.github.com>
2024-11-10 01:23:36 +0300

    Update installarch.sh

--- installarch.sh
+++ installarch.sh
@@ -6,7 +6,7 @@ read -p "hostname (HOST): " HOST
 read -p "path to install (e.g., /dev/sda1): " PATH
 
 pacman -Sy
-pacman -S archlinux-keyring --no-confirm
+pacman -S archlinux-keyring --noconfirm
 
 mkfs.btrfs -L arch $PATH
 mount $PATH /mnt
@@ -20,43 +20,25 @@ mount -o subvol=/@,noatime,compress=lzo $PATH /mnt
 mkdir /mnt/home
 mount -o subvol=/@home,noatime,compress=lzo $PATH /mnt/home
 
-echo "liquorix"
-pacman-key --keyserver hkps://keyserver.ubuntu.com --recv-keys 9AE4078033F8024D
-pacman-key --lsign-key 9AE4078033F8024D
-
-echo -e '\n[liquorix]\nServer = https://liquorix.net/archlinux/$repo/$arch' | sudo tee -a '/etc/pacman.conf' > /dev/null
-
-echo "chaotic-aur"
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key 3056513887B78AEB
+pacman-key --keyserver hkps://keyserver.ubuntu.com --recv-keys 9AE4078033F8024D 3056513887B78AEB
+pacman-key --lsign-key 9AE4078033F8024D 3056513887B78AEB
 
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
+echo -e '\n[liquorix]\nServer = https://liquorix.net/archlinux/$repo/$arch' | sudo tee -a '/etc/pacman.conf' > /dev/null
 echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a '/etc/pacman.conf' > /dev/null
 
 sudo sed -i 's/^#*\(ParallelDownloads\s*=\s*\).*/\110/' /etc/pacman.conf
 
 pacman -Sy
 
-pacstrap /mnt base base-devel linux-lqx linux-lqx-headers linux-firmware networkmanager intel-ucode git nano curl wget zsh yay --no-confirm
+pacstrap /mnt base base-devel linux-lqx linux-lqx-headers linux-firmware networkmanager chaotic-keyring chaotic-mirrorlist intel-ucode git nano curl wget zsh yay --needed --noconfirm
 genfstab -U /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt
 
-echo "liquorix"
-pacman-key --keyserver hkps://keyserver.ubuntu.com --recv-keys 9AE4078033F8024D
-pacman-key --lsign-key 9AE4078033F8024D
-
 echo -e '\n[liquorix]\nServer = https://liquorix.net/archlinux/$repo/$arch' | sudo tee -a '/etc/pacman.conf' > /dev/null
-
-echo "chaotic-aur"
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key 3056513887B78AEB
-
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-
 echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a '/etc/pacman.conf' > /dev/null
 
 sed -i 's/^#*\(ParallelDownloads\s*=\s*\).*/\110/' /etc/pacman.conf
@@ -76,7 +58,7 @@ echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
 passwd
 
-useradd -mG wheel -s /usr/bin/zsh $USER
+useradd -mG wheel -s /bin/zsh $USER
 passwd $USER
 echo "$USER ALL=(ALL:ALL) ALL" >> /etc/sudoers
 visudo -c
@@ -86,6 +68,12 @@ sudo pacman -Sy grub
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
-sudo pacman -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter xorg network-manager-applet
+sudo pacman -S xfce4 (garcon thunar thunar-volman tumbler xfce4-appfinder 
+xfce4-panel xfce4-power-manager xfce4-session xfce4-settings xfconf xfdesktop xfwm4) [2,3,4,5,6,7,8,9,10,11,12,13]
+ 
+xfce4-goodies (mousepad thunar-media-tags-plugin xfce4-battery-plugin xfce4-clipman-plugin 
+xfce4-pulseaudio-plugin xfce4-taskmanager xfce4-whiskermenu-plugin) [1, 5, 8, 9, 24, 30, 36]
+
+sudo pacman -S lightdm lightdm-gtk-greeter xorg network-manager-applet --needed
 exit
 umount -R /mnt

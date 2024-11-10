868ac62f2a40d6c4b9d3779889fc28765e60d195
DavidsTens <134627046+DavidsTens@users.noreply.github.com>
2024-11-10 03:57:13 +0300

    Update installarch.sh

--- installarch.sh
+++ installarch.sh
@@ -10,7 +10,7 @@ read -p "path to install (e.g., /dev/sda1): " DRIVE
 pacman -Sy
 pacman -S archlinux-keyring --noconfirm
 
-mkfs.btrfs -L arch $DRIVE
+mkfs.btrfs -f -L arch $DRIVE
 mount $DRIVE /mnt
 
 btrfs su cr /mnt/@
@@ -25,52 +25,38 @@ mount -o subvol=/@home,noatime,compress=lzo $DRIVE /mnt/home
 pacman-key --keyserver hkps://keyserver.ubuntu.com --recv-keys 9AE4078033F8024D 3056513887B78AEB
 pacman-key --lsign-key 9AE4078033F8024D 3056513887B78AEB
 
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-
-echo -e '\n[liquorix]\nServer = https://liquorix.net/archlinux/$repo/$arch' | sudo tee -a '/etc/pacman.conf' > /dev/null
-echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a '/etc/pacman.conf' > /dev/null
-
-sudo sed -i 's/^#*\(ParallelDownloads\s*=\s*\).*/\110/' /etc/pacman.conf
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
+echo -e '\n[liquorix]\nServer = https://liquorix.net/archlinux/$repo/$arch\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf > /dev/null && sudo sed -i 's/^#*\(ParallelDownloads\s*=\s*\).*/\110/' /etc/pacman.conf
 
 pacman -Sy
 
 pacstrap /mnt base base-devel linux-lqx linux-lqx-headers linux-firmware networkmanager chaotic-keyring chaotic-mirrorlist intel-ucode git nano curl zsh aura --needed --noconfirm
 genfstab -U /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt
+arch-chroot /mnt bash -c "
+echo -e '\n[liquorix]\nServer = https://liquorix.net/archlinux/\$repo/\$arch\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | tee -a /etc/pacman.conf > /dev/null &&
+sed -i 's/^#*\(ParallelDownloads\s*=\s*\).*/\110/' /etc/pacman.conf &&
+echo \$HOST > /etc/hostname &&
+echo -e '127.0.0.1 localhost\n::0 localhost\n127.0.0.1 \$HOST' >> /etc/hosts &&
+systemctl enable NetworkManager &&
+systemctl mask NetworkManager-wait-online.service &&
+ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime &&
+hwclock --systohc &&
+sed -i 's/#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen &&
+locale-gen &&
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf &&
+echo 'Set root password' &&
+passwd &&
+useradd -mG wheel -s /bin/zsh \$USER &&
+echo 'Set password for user \$USER' &&
+passwd \$USER &&
+echo '\$USER ALL=(ALL:ALL) ALL' >> /etc/sudoers &&
+visudo -c &&
+sudo pacman -Sy grub --noconfirm &&
+grub-install /dev/sda &&
+grub-mkconfig -o /boot/grub/grub.cfg &&
+sudo pacman -S garcon thunar thunar-volman tumbler xfce4-appfinder xfce4-panel xfce4-power-manager xfce4-session xfce4-settings xfconf xfdesktop xfwm4 mousepad thunar-media-tags-plugin xfce4-battery-plugin xfce4-clipman-plugin xfce4-pulseaudio-plugin xfce4-taskmanager xfce4-whiskermenu-plugin --noconfirm &&
+sudo pacman -S lightdm lightdm-gtk-greeter st xorg network-manager-applet --needed --noconfirm
+"
 
-echo -e '\n[liquorix]\nServer = https://liquorix.net/archlinux/$repo/$arch' | sudo tee -a '/etc/pacman.conf' > /dev/null
-echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a '/etc/pacman.conf' > /dev/null
-
-sed -i 's/^#*\(ParallelDownloads\s*=\s*\).*/\110/' /etc/pacman.conf
-
-echo $HOST > /etc/hostname
-echo -e "127.0.0.1 localhost\n::0 localhost\n127.0.0.1 $HOST" >> /etc/hosts
-
-systemctl enable NetworkManager
-systemctl mask NetworkManager-wait-online.service
-
-ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
-hwclock --systohc
-
-sed -i "s/#en_US.UTF-8/en_US.UTF-8/g" /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-
-passwd
-
-useradd -mG wheel -s /bin/zsh $USER
-passwd $USER
-echo "$USER ALL=(ALL:ALL) ALL" >> /etc/sudoers
-visudo -c
-
-sudo pacman -Sy grub
-
-grub-install /dev/sda
-grub-mkconfig -o /boot/grub/grub.cfg
-
-sudo pacman -S xfce4 garcon thunar thunar-volman tumbler xfce4-appfinder xfce4-panel xfce4-power-manager xfce4-session xfce4-settings xfconf xfdesktop xfwm4 mousepad thunar-media-tags-plugin xfce4-battery-plugin xfce4-clipman-plugin xfce4-pulseaudio-plugin xfce4-taskmanager xfce4-whiskermenu-plugin
-sudo pacman -S lightdm lightdm-gtk-greeter st xorg network-manager-applet --needed
-exit
 umount -R /mnt
